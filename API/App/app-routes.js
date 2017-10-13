55923961269f336ca17aac13f23368f727d44967
iagoguimaraes <iagog6@hotmail.com>
2017-10-13 18:55:05 -0300

    done: biblioteca route do angular todo: esquema de routing

--- API/App/app-routes.js
+++ API/App/app-routes.js
@@ -1,1 +1,21 @@
-﻿
+﻿angular.module('app', ['ngRoute']).config(function ($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl: 'Views/Home.html',
+            controller: 'HomeController'
+        })
+
+        // route for the about page
+        .when('/Dashboard/Mktzap', {
+            templateUrl: 'Views/Dashboard/Mktzap.html',
+            controller: 'MktzapController'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl: 'pages/contact.html',
+            controller: 'contactController'
+        });
+});
