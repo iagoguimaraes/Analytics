44cfb70c5ab170bd12be6a6875867169ebe0dc8c
iagoguimaraes <iagog6@hotmail.com>
2017-10-18 11:55:30 -0200

    done: esquema de cache dos html no route done: ajuste de margin dash mktzap

--- API/App/app-routes.js
+++ API/App/app-routes.js
@@ -1,17 +1,17 @@
 ﻿angular.module('app').config(function ($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: '/App/Views/Home/Home.html',
+            templateUrl: '/App/Views/Home/Home.html?v=' + new Date().getTime(),
             controller: 'ctrl-home'
         })
 
         .when('/Mktzap/Dashboard', {
-            templateUrl: '/App/Views/Mktzap/Dashboard.html',
+            templateUrl: '/App/Views/Mktzap/Dashboard.html?v=' + new Date().getTime(),
             controller: 'ctrl-mktzap-dashboard'
         })
 
         .when('/Mktzap/Relatorio', {
-            templateUrl: '/App/Views/Mktzap/Relatorio.html',
+            templateUrl: '/App/Views/Mktzap/Relatorio.html?v=' + new Date().getTime(),
             controller: 'ctrl-mktzap-relatorio'
         })
 
