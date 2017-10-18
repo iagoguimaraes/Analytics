9eaa6770348ca60a49d43de51091f707a2c84545
iagoguimaraes <iagog6@hotmail.com>
2017-10-18 11:35:51 -0200

    done: diretiva bar char done: grafico de barra no dash mkt

---
+++ API/App/Directives/barChart.js
@@ -0,0 +1,51 @@
+﻿angular.module('app').directive('barChart', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            dados: '=',
+            titulo: '@',
+            subtitulo: '@',
+            height: '@',
+            yAxisTitle: '@',
+            seriesName: '@',
+        },
+        template: '<div></div>',
+        link: function (scope, element) {
+            scope.$watch('dados', function (dados) {
+                Highcharts.chart(element[0], {
+                    chart: {
+                        type: 'bar'
+                    },
+                    title: {
+                        text: scope.titulo
+                    },
+                    subtitle: {
+                        text: scope.subtitulo
+                    },
+                    xAxis: {
+                        categories: dados.map(obj => obj[Object.keys(obj)[0]]),
+                        crosshair: true
+                    },
+                    yAxis: {
+                        title: {
+                            text: scope.yAxisTitle
+                        }
+                    },
+                    series: [{
+                        name: scope.seriesName,
+                        data: dados.map(obj => obj[Object.keys(obj)[1]])
+                    }],
+                    plotOptions: {
+                        bar: {
+                            dataLabels: {
+                                enabled: true,
+                            }
+                        }
+                    }
+
+                });
+            });
+        }
+    }
+});
