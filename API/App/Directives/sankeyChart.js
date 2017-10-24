a75f5563f2f2bb1c5e2b4f623cb3fb0148d451f0
iagoguimaraes <iagog6@hotmail.com>
2017-10-24 16:05:46 -0200

    add: sankey chart done: sankey no dash tim

---
+++ API/App/Directives/sankeyChart.js
@@ -0,0 +1,38 @@
+﻿angular.module('app').directive('sankeyChart', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            dados: '=',
+            titulo: '@',
+            subtitulo: '@',
+            height: '@',
+            seriesName: '@',
+        },
+        template: '<div></div>',
+        link: function (scope, element) {
+            scope.$watch('dados', function (dados) {
+
+                Highcharts.chart(element[0], {
+                    chart: {
+                        height: scope.height,
+                    },
+                    title: {
+                        text: scope.titulo
+                    },
+                    subtitle: {
+                        text: scope.subtitulo
+                    },
+                    series: [{
+                        keys: ['from', 'to', 'weight'],
+                        data: dados.map(obj =>[obj['de'], obj['para'], obj['qtd']]),
+                        type: 'sankey',
+                        name: scope.seriesName
+                    }]
+
+                });
+
+            });
+        }
+    }
+});
