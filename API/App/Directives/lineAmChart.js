aaf6d985ac807ba517cae14417844b9b8750d996
iagoguimaraes <iagog6@hotmail.com>
2017-10-25 11:32:23 -0200

    add: amCharts done: promessa/hora tim

---
+++ API/App/Directives/lineAmChart.js
@@ -0,0 +1,37 @@
+﻿angular.module('app').directive('lineAmChart', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            dados: '=',
+        },
+        template: '<div></div>',
+        link: function (scope, element) {
+            scope.$watch('dados', function (dados) {
+                AmCharts.makeChart(element[0], {
+                    type: "serial",
+                    theme: "light", 
+                    dataProvider: dados,                   
+                    categoryField: "hora",                    
+                    graphs: [
+                        {
+                            valueField: "promessa",
+                            bullet: "round",
+                            dashLength: 2,
+                            bulletSize: 15,
+                            bulletColor: "#8d83c8",
+                            lineColor: "#5bb5ea",                   
+                        }
+                    ],
+                    chartCursor: {
+                        fullWidth: true,
+                        cursorColor: "#FFFFFF",
+                        categoryBalloonColor: "#8d83c8",
+                        balloonPointerOrientation: "vertical"                        
+                    },
+                });
+
+            });
+        }
+    }
+});
