60c4f8671815271155514801fef83a7c96d00876
iagoguimaraes <iagog6@hotmail.com>
2017-11-06 12:01:00 -0200

    titulos btcs subtitulo home

--- API/App/Controllers/Home/HomeCtrl.js
+++ API/App/Controllers/Home/HomeCtrl.js
@@ -1,4 +1,4 @@
 ﻿angular.module('app').controller('ctrl-home', function ($scope, $http, dadosPagina) {
     dadosPagina.titulo = 'Página Inicial'
-    dadosPagina.subTitulo = 'Seja Bem-Vindo!'
+    dadosPagina.descricao = 'Seja Bem-Vindo!'
 });
