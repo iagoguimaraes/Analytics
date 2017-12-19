a7a3c8391c3e3ef8a06b99630fd730f4b5405e4a
iagoguimaraes <iagog6@hotmail.com>
2017-12-19 17:56:41 -0200

    bubble charts testes no dash lote vivo

--- API/App/Controllers/Vivo/LoteCtrl.js
+++ API/App/Controllers/Vivo/LoteCtrl.js
@@ -3,8 +3,12 @@
     dadosPagina.descricao = 'Vivo'
 
     $scope.dashboard = {
-        filtro: {
-            dia: 10
+        comparativo: {
+            dia: 2,
+            x: '%discado',
+            y: '%promessa',
+            z: 'total',
+            indicador: '%discado'
         }
     };
 
@@ -17,17 +21,60 @@
             $scope.dashboard.status = r.status;
             $scope.dashboard.dados = r.data.Table;
 
-            $scope.filtrarDados();
+            $scope.filtrarColumn();
+            $scope.filtrarBubble();
         });
     }
 
-    $scope.filtrarDados = function () {
-        if ($scope.dashboard.filtro.dia)
-            $scope.dashboard.grafico = $scope.dashboard.dados
-                .filter(o => o.dia == $scope.dashboard.filtro.dia)
-                .map(o => ({lote: o.data_inclusao, discado: (o.discado/o.total).toFixed(1)*100}));
+    $scope.filtrarColumn = function () {
+        $scope.dashboard.comparativo.column = $scope.dashboard.dados
+            .filter(o => o.dia == $scope.dashboard.comparativo.dia)
+            .map(o => ({
+                name: o.data_inclusao,
+                value: calcular($scope.dashboard.comparativo.indicador, o),
+            }));
+
+    };
+
+    $scope.filtrarBubble = function () {
+        $scope.dashboard.comparativo.bubble = $scope.dashboard.dados
+            .filter(o => o.dia == $scope.dashboard.comparativo.dia)
+            .map(o => ({
+                name: o.data_inclusao,
+                x: calcular($scope.dashboard.comparativo.x, o),
+                y: calcular($scope.dashboard.comparativo.y, o),
+                z: calcular($scope.dashboard.comparativo.z, o)
+            }));
     };
 
     $scope.carregarDashboard();
 
 });
+
+function calcular(indicador, o) {
+    switch (indicador) {
+        case '%discado':
+            return parseInt(o.discado * 100 / o.total);
+            break;
+        case '%contato':
+            return parseInt(o.contato * 100 / o.total);
+            break;
+        case '%cpc':
+            return parseInt(o.cpc * 100 / o.total);
+            break;
+        case '%cpca':
+            return parseInt(o.cpca * 100 / o.total);
+            break;
+        case '%promessa':
+            return parseInt(o.promessa * 100 / o.total);
+            break;
+        case 'conversao':
+            return parseInt(o.promessa * 100 / o.cpca);
+            break;
+        case 'spin':
+            return Math.round(o.ttdiscado / o.total * 1e1) / 1e1;
+            break;
+        default:
+            return o[indicador];
+    }
+}
---
+++ API/App/Directives/bubbleChart.js
@@ -0,0 +1,70 @@
+﻿angular.module('app').directive('bubbleChart', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            dados: '=',
+            height: '@',
+            tituloX: '=',
+            tituloY: '=',
+            tituloZ: '=',
+        },
+        template: '<div></div>',
+        link: function (scope, element) {
+            scope.$watch('dados', function (dados) {
+                if (dados) {
+
+                    Highcharts.chart(element[0], {
+                        chart: {
+                            type: 'bubble',
+                            height: scope.height
+                        },
+                        title: {
+                            text: ''
+                        },
+                        legend: {
+                            enabled: false
+                        },
+                        xAxis: {
+                            title: {
+                                text: scope.tituloX
+                            }
+                        },
+                        yAxis: {
+                            title: {
+                                text: scope.tituloY
+                            }
+                        },
+                        series: [{
+                            data: scope.dados
+
+                        }],
+                        plotOptions: {
+                            series: {
+                                dataLabels: {
+                                    enabled: true,
+                                    format: '{point.name}'
+                                }
+                            }
+                        },
+                        tooltip: {
+                            useHTML: true,
+                            headerFormat: '',
+                            pointFormat:'<h4>{point.name}</h4>' + 
+                                '<p><b>' + scope.tituloX + ':</b> {point.x}</p>' +
+                                '<p><b>' + scope.tituloY + ':</b> {point.y}</p>' +
+                                '<p><b>' + scope.tituloZ + ':</b> {point.z}</p>',
+                            followPointer: true
+                        },
+                        credits: {
+                            enabled: false
+                        },
+
+                    });
+
+                }
+
+            });
+        }
+    }
+});
