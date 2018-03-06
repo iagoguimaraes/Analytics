16367ded14509c513f93d598c0c188de19baab29
iago.guimaraes <iago.guimaraes@DES05-DIG>
2018-03-06 17:27:16 -0300

    carrefour producao

--- Analytics/Controllers/CarrefourController.cs
+++ Analytics/Controllers/CarrefourController.cs
@@ -63,5 +63,35 @@ namespace Analytics.Controllers
             }
         }
 
+        [Route("dashboard/producao")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage DashboardProducao(FormDataCollection form)
+        {
+            try
+            {
+                DateTime dtini = Convert.ToDateTime(form["dtini"]);
+                DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+                DataTable credores = JsonConvert.DeserializeObject<DataTable>(form["credores"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_CARREFOUR"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+                    parametros.Add("credores", credores);
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_producao", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
     }
 }
