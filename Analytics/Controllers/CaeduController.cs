f4127d627e3d2fa41b93af17ce412ab5549736fa
victor.galvao <victor.galvao@GALVAO-NOTE>
2018-05-11 15:34:35 -0300

    Ajuste producao humano Caedu.

--- Analytics/Controllers/CaeduController.cs
+++ Analytics/Controllers/CaeduController.cs
@@ -79,6 +79,8 @@ namespace Analytics.Controllers
             {
                 DateTime dtini = Convert.ToDateTime(form["dtini"]);
                 DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+                DataTable segmento = JsonConvert.DeserializeObject<DataTable>(form["segmento"]);
+                DataTable faixa = JsonConvert.DeserializeObject<DataTable>(form["faixa"]);
 
                 using (SqlHelper sql = new SqlHelper("CUBO_CAEDU"))
                 {
@@ -86,6 +88,8 @@ namespace Analytics.Controllers
 
                     parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
                     parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+                    parametros.Add("segmento", segmento);
+                    parametros.Add("faixa", faixa);
 
                     DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_producao_humano", parametros);
                     return Request.CreateResponse(HttpStatusCode.OK, resultado);
