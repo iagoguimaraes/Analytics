2b484f269e6058fcfb93eb7784a8e372974879f9
iago.guimaraes <iago.guimaraes@DES05-DIG>
2018-08-21 17:57:16 -0300

    integração com powerbi

--- Analytics/Controllers/AnalyticsController.cs
+++ Analytics/Controllers/AnalyticsController.cs
@@ -1,44 +1,16 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Formatting;
 using System.Web.Http;
+using System.Threading.Tasks;
 
 namespace Analytics.Controllers
 {
     [RoutePrefix("api/analytics")]
     public class AnalyticsController : ApiController
     {
-        [Route("getPaginas")]
-        [HttpPost]
-        [Autorizar]
-        [Gravar]
-        public HttpResponseMessage GetPagina()
-        {
-            try
-            {
-                Sessao sessao = (Sessao)Request.Properties["Sessao"];
-
-                using (SqlHelper sql = new SqlHelper())
-                {
-                    Dictionary<string, object> parametros = new Dictionary<string, object>();
-
-                    parametros.Add("id_usuario", sessao.id_usuario.ToString());
-
-                    DataTable paginas = sql.ExecuteProcedureDataTable("sp_sel_acessoPagina", parametros);
-                    return Request.CreateResponse(HttpStatusCode.OK, paginas);
-                }               
-            }
-            catch (Exception e)
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
-            }
-        }
-
         [Route("paginainicial")]
         [HttpPost]
         [Autorizar]
@@ -91,5 +63,24 @@ namespace Analytics.Controllers
             }
         }
 
+        [Route("requisicoes")]
+        [HttpPost]
+        [Autorizar]
+        public async Task<HttpResponseMessage> Requisicoes()
+        {
+            try
+            {
+                string WorkspaceId = "544d9d10-0068-480c-a7f6-6bf26c2c6279";
+                string ReportId = "0baf7dd7-17fb-4545-8b8c-95c0580a7e70";
+
+                PowerBI powerbi = new PowerBI();
+                EmbedConfig dashboard = await powerbi.CarregarDashboard(WorkspaceId, ReportId);
+                return Request.CreateResponse(HttpStatusCode.OK, dashboard);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
     }
 }
