9b1a3a0ec34b95b8a65703898877d5446d80e993
iago.guimaraes <iago.guimaraes@DES05-DIG>
2018-08-28 16:00:37 -0300

    dashboard mktzap gerencial

---
+++ Analytics/App/Directives/canvasjs.js
@@ -0,0 +1,21 @@
+﻿angular.module('app').directive('canvasjs', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            config: '=',
+        },
+        template: '<div></div>',
+        link: function (scope, element) {
+            scope.$watch('config', function () {
+                let chart = new CanvasJS.Chart(element[0], scope.config);
+                chart.render();               
+            });
+        }
+    }
+});
+
+
+
+
+
