9985926d7e11f9a6f8f466c0a97d5316863ed2d1
dacio.silva <dacio.silva@creditcash.com.br>
2018-08-29 19:13:49 -0300

    acionamento vivo final, method gerar arquivo response

---
+++ Analytics/Models/GerarArquivo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace Analytics.Models
+{
+    public class GerarArquivo
+    {
+
+        public GerarArquivo(HttpResponse Response, DataTable dt)
+        {
+            try
+            {
+                string[] columnNames = dt.Columns.Cast<DataColumn>().
+                                  Select(column => column.ColumnName).
+                                  ToArray();
+                Response.Output.Write(string.Join(";", columnNames));
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    string[] fields = row.ItemArray.Select(field => field.ToString()).ToArray();
+                    Response.Output.Write("\r\n" + string.Join(";", fields));
+                }
+
+                Response.Flush();
+                Response.End();
+
+            }
+            catch (Exception)
+            {
+
+                throw new NotImplementedException();
+            }
+
+
+        }
+    }
+}
