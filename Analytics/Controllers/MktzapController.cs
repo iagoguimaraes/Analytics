2dc10ad34b4a7e00e621d90132f2aede10469122
iago.guimaraes <iago.guimaraes@DES05-DIG>
2018-10-17 17:09:24 -0300

    mktzap dashboard operacional

--- Analytics/Controllers/MktzapController.cs
+++ Analytics/Controllers/MktzapController.cs
@@ -147,5 +147,40 @@ namespace Analytics.Controllers
             }
         }
 
+        [Route("operacional")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage Operacional(FormDataCollection form)
+        {
+            try
+            {
+                string dtini = form["dtini"];
+                string dtfim = form["dtfim"];
+                string campanhas = form["campanhas"];
+                string setores = form["setores"];
+
+                DataTable _campanhas = JsonConvert.DeserializeObject<DataTable>(campanhas);
+                DataTable _setores = JsonConvert.DeserializeObject<DataTable>(setores);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_MKTZAP"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini);
+                    parametros.Add("dtfim", dtfim);
+                    parametros.Add("campanhas", _campanhas);
+                    parametros.Add("setores", _setores);
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_operacional", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
     }
 }
