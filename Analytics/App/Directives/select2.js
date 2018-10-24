15147334a26d69274425d5cec483608a96303c76
victor.galvao <victor.galvao@GALVAO-NOTE>
2018-10-24 16:37:50 -0300

    Ajustes no formulario de Usuarios.

--- Analytics/App/Directives/select2.js
+++ Analytics/App/Directives/select2.js
@@ -9,14 +9,17 @@ angular.module('app').directive('select2', function () {
         },
         template: '<select class="select2" multiple="multiple" readonly="true"></select>',
         link: function (scope, element, attr, ctrl) {
+            var dados = scope.dados;
+            
             $(element[0]).select2({
                 data: scope.dados.map(obj => ({ 'id': obj[Object.keys(obj)[0]], 'text': obj[Object.keys(obj)[1]] })),
                 allowClear: true,
+               
             });
 
             // transforma array em array de objeto
             element.bind('change', function () {
-                let arrayObjt = ctrl.$modelValue.map(obj => ({ id: obj }));
+                let arrayObjt = ctrl.$viewValue.map(obj => ({ id: obj }));
                 let viewValue = arrayObjt.length ? JSON.stringify(arrayObjt) : null; // ou retorna lista com itens ou null (serializador C#)
                 ctrl.$setViewValue(viewValue);
             });
