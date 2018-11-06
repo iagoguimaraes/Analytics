2fdfac89d0dbf735368459a97735cade826f95f2
dacio.silva <dacio.silva@creditcash.com.br>
2018-11-06 18:38:02 -0200

    efetividade vivo hum

--- Analytics/App/Directives/mapaBrasil.js
+++ Analytics/App/Directives/mapaBrasil.js
@@ -2,11 +2,12 @@
     return {
         restrict: 'E',
         replace: true,
+        require: '^?ngModel',
         scope: {
             dados: '=',
         },
         template: '<div></div>',
-        link: function (scope, element) {
+        link: function (scope, element, attrs, ngModelCtrl) {
             scope.$watch('dados', function (dados) {
                 if (dados) {
                     AmCharts.makeChart(element[0], {
@@ -20,8 +21,9 @@
                             areas: dados
                         },
                         areasSettings: {
-                            autoZoom: true,
-                            balloonText: "[[title]]: <strong>[[value]]</strong>",
+                            autoZoom: false,
+                            selectable: true,
+                            balloonText: "[[title]]: <strong>[[value]] </strong>", //[[percent]]%
                         },
                         valueLegend: {
                             right: 10,                          
@@ -29,9 +31,14 @@
                         zoomControl: {
                             minZoomLevel: 0.9
                         },
-                    });
+                    }).addListener("clickMapObject", function (event) {
+                        //document.getElementById("info").innerHTML = 'Clicked ID: ' + event.mapObject.id + ' (' + event.mapObject.title + ')';
+                        ngModelCtrl.$setViewValue(event.mapObject.id);
+                    });                  
+
                 }               
             });
+
         }
     }
 });
