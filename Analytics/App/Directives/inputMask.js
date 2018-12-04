7c99d366b0d98708dc9c7c4f3f1b07ae3719caf8
iago.guimaraes <iago.guimaraes@DES05-DIG>
2018-12-04 18:57:40 -0200

    riachuelo projecao

---
+++ Analytics/App/Directives/inputMask.js
@@ -0,0 +1,14 @@
+﻿angular.module('app').directive('inputMask', function () {
+    return {
+        restrict: 'E',
+        replace: true,    
+        scope: {
+            mask: '@',
+            reverse: '@'
+        },
+        template: '<input type="text"/>',
+        link: function (scope, element, attr, ctrl) {
+            $(element).mask(scope.mask, {reverse: scope.reverse});
+        }
+    };
+});
