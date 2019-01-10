64d1d6b1ef4a761ed2b90d8dad3e28f5103dfeb5
Dácio Silva <dacio.silva@creditcash.com.br>
2019-01-10 12:33:38 -0200

    ocupação por fila, tim dig

--- Analytics/Controllers/TimController.cs
+++ Analytics/Controllers/TimController.cs
@@ -390,6 +390,7 @@ namespace Analytics.Controllers
                 DateTime dtini = Convert.ToDateTime(form["dtini"]);
                 DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
                 DataTable produtos = JsonConvert.DeserializeObject<DataTable>(form["produtos"]);
+                DataTable fila = JsonConvert.DeserializeObject<DataTable>(form["fila"]);
 
                 using (SqlHelper sql = new SqlHelper("CUBO_TIM"))
                 {
@@ -398,6 +399,7 @@ namespace Analytics.Controllers
                     parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
                     parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
                     parametros.Add("produtos", produtos);
+                    parametros.Add("fila", fila);
 
                     DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_ocupacao", parametros);
                     return Request.CreateResponse(HttpStatusCode.OK, resultado);
