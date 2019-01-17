1cfe63390f2c907924a74fabf055a9bcac1920a4
iago.guimaraes <iago.guimaraes@DES05-DIG>
2019-01-17 10:22:28 -0200

    dashboard backoffice tim vendas

--- Analytics/Controllers/TimController.cs
+++ Analytics/Controllers/TimController.cs
@@ -1044,6 +1044,34 @@ namespace Analytics.Controllers
             }
         }
 
+        [Route("vendas/backoffice")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage VendasBackoffice(FormDataCollection form)
+        {
+            try
+            {
+                DateTime dtini = Convert.ToDateTime(form["dtini"]);
+                DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_TIM_VENDAS"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_backoffice", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
         #endregion
 
     }
