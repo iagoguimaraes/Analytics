23b8eca5e445e45178c9e15be9cd2134a6a44215
Dácio Silva <dacio.silva@creditcash.com.br>
2019-02-11 12:07:38 -0200

    page promessa sms e retorno - localiza

--- Analytics/Controllers/LocalizaController.cs
+++ Analytics/Controllers/LocalizaController.cs
@@ -95,6 +95,118 @@ namespace Analytics.Controllers
             }
         }
 
+        [Route("dashboard/promessasms")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage DashboardPromessaSMS(FormDataCollection form)
+        {
+            try
+            {
+                DateTime dtini = Convert.ToDateTime(form["dtini"]);
+                DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_LOCALIZA"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_promessaSMS", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Route("dashboard/checksms")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage CheckSMS(FormDataCollection form)
+        {
+            try
+            {
+                string id_chamada = form["id_chamada"];
+                bool sms = Convert.ToBoolean(form["sms"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_LOCALIZA"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("id_chamada", id_chamada);
+                    parametros.Add("sms", Convert.ToInt16(sms).ToString());
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_upd_smsChamada", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Route("dashboard/retorno")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage DashboardRetorno(FormDataCollection form)
+        {
+            try
+            {
+                DateTime dtini = Convert.ToDateTime(form["dtini"]);
+                DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_LOCALIZA"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_retorno", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Route("dashboard/checkretorno")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage CheckRetorno(FormDataCollection form)
+        {
+            try
+            {
+                string id_chamada = form["id_chamada"];
+                bool retorno = Convert.ToBoolean(form["retorno"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_LOCALIZA"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("id_chamada", id_chamada);
+                    parametros.Add("retorno", Convert.ToInt16(retorno).ToString());
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_upd_retornoChamada", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
         #endregion
 
 
