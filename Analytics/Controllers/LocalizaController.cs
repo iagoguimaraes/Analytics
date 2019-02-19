ef65ff3c683cbe762ba3c0d4d29b9ddc733bec60
Dácio Silva <dacio.silva@creditcash.com.br>
2019-02-19 17:18:00 -0300

    dash pagamento localiza

--- Analytics/Controllers/LocalizaController.cs
+++ Analytics/Controllers/LocalizaController.cs
@@ -233,6 +233,34 @@ namespace Analytics.Controllers
             }
         }
 
+        [Route("dashboard/pagamento")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage DashboardPagamento(FormDataCollection form)
+        {
+            try
+            {
+                DateTime dtini = Convert.ToDateTime(form["dtini"]);
+                DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_LOCALIZA"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_pagamento", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
 
         #endregion
 
