dd35491aa31c87cfbe4ee149bfd02e5aa37aa608
victor.galvao <victor.galvao@GALVAO-NOTE>
2019-02-21 12:20:35 -0300

    Inclusão tim vendas retorno.

--- Analytics/Controllers/TimController.cs
+++ Analytics/Controllers/TimController.cs
@@ -1092,6 +1092,62 @@ namespace Analytics.Controllers
             }
         }
 
+        [Route("vendas/ura")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage DashboardPromessaSMS(FormDataCollection form)
+        {
+            try
+            {
+                DateTime dtini = Convert.ToDateTime(form["dtini"]);
+                DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_TIM_VENDAS"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_retornoUra", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Route("vendas/checkretorno")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage CheckSMS(FormDataCollection form)
+        {
+            try
+            {
+                string id_chamada = form["id_chamada"];
+                bool sms = Convert.ToBoolean(form["sms"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_TIM_VENDAS"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("id_chamada", id_chamada);
+                    parametros.Add("sms", Convert.ToInt16(sms).ToString());
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_upd_retornoChamada", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
         #endregion
 
     }
