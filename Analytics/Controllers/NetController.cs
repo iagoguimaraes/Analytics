6eccb606257e74be3acd7ac5a2553a3365495d74
victor.galvao <victor.galvao@GALVAO-NOTE>
2019-02-22 11:44:56 -0300

    inclusao filtro carteira.

--- Analytics/Controllers/NetController.cs
+++ Analytics/Controllers/NetController.cs
@@ -44,6 +44,7 @@ namespace Analytics.Controllers
                 DateTime dtini = Convert.ToDateTime(form["dtini"]);
                 DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
                 DataTable empresas = JsonConvert.DeserializeObject<DataTable>(form["empresas"]);
+                DataTable carteiras = JsonConvert.DeserializeObject<DataTable>(form["carteiras"]);
                 DataTable supervisores = JsonConvert.DeserializeObject<DataTable>(form["supervisores"]);
                 DataTable equipes = JsonConvert.DeserializeObject<DataTable>(form["equipes"]);
                 int horaini = Convert.ToInt16(form["horaini"]);
@@ -57,6 +58,7 @@ namespace Analytics.Controllers
                     parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
                     parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
                     parametros.Add("empresas", empresas);
+                    parametros.Add("carteiras", carteiras);
                     parametros.Add("supervisores", supervisores);
                     parametros.Add("equipes", equipes);
                     parametros.Add("horaini", horaini);
@@ -83,6 +85,7 @@ namespace Analytics.Controllers
                 DateTime dtini = Convert.ToDateTime(form["dtini"]);
                 DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
                 DataTable empresas = JsonConvert.DeserializeObject<DataTable>(form["empresas"]);
+                DataTable carteiras = JsonConvert.DeserializeObject<DataTable>(form["carteiras"]);
                 DataTable supervisores = JsonConvert.DeserializeObject<DataTable>(form["supervisores"]);
                 DataTable equipes = JsonConvert.DeserializeObject<DataTable>(form["equipes"]);
 
@@ -93,6 +96,7 @@ namespace Analytics.Controllers
                     parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
                     parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
                     parametros.Add("empresas", empresas);
+                    parametros.Add("carteiras", carteiras);
                     parametros.Add("supervisores", supervisores);
                     parametros.Add("equipes", equipes);
 
