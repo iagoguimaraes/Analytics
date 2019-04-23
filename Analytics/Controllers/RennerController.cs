67578c5222c68c0f6124028e49df416025375eda
iago.guimaraes <iago.guimaraes@DES05-DIG>
2019-04-23 15:29:14 -0300

    acao massiva renner humano

--- Analytics/Controllers/RennerController.cs
+++ Analytics/Controllers/RennerController.cs
@@ -269,6 +269,8 @@ namespace Analytics.Controllers
             }
         }
 
+        #region Humano
+
         [Route("dashboard/humano/filtros")]
         [HttpGet]
         [Autorizar]
@@ -592,6 +594,41 @@ namespace Analytics.Controllers
                 return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
             }
         }
+
+        [Route("dashboard/humano/acaomassiva")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage DashboardHumanoAcaoMassiva(FormDataCollection form)
+        {
+            try
+            {
+                DateTime dtini = Convert.ToDateTime(form["dtini"]);
+                DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+                DataTable produtos = JsonConvert.DeserializeObject<DataTable>(form["produtos"]);
+                DataTable atrasos = JsonConvert.DeserializeObject<DataTable>(form["faixas"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_RENNER"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+                    parametros.Add("produtos", produtos);
+                    parametros.Add("faixas", atrasos);
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_humano_dashboard_acaomassiva", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        #endregion
+
     }
 
 
