a0b46a7b43dc02b18392fb8295938c69298fbf72
victor.galvao <victor.galvao@GALVAO-NOTE>
2019-05-10 12:23:59 -0300

    Ajuste downloads Bradesco.

--- Analytics/Controllers/BradescoController.cs
+++ Analytics/Controllers/BradescoController.cs
@@ -424,6 +424,34 @@ namespace Analytics.Controllers
             }
         }
 
+        [Route("dashboard/receptivo/producao")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage DashboardProducaoRec(FormDataCollection form)
+        {
+            try
+            {
+                DateTime dtini = Convert.ToDateTime(form["dtini"]);
+                DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_BRADESCO"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_producao_receptivo", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
 
         [Route("dashboard/eavm/producao")]
         [HttpPost]
