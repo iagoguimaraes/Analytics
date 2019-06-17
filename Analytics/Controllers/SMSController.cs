27f6dc267f67f5050371dac29a14b9cf02946140
iago.guimaraes <iago.guimaraes@DES05-DIG>
2019-06-17 17:40:14 -0300

    esqueleto projeto SMS

---
+++ Analytics/Controllers/SMSController.cs
@@ -0,0 +1,38 @@
+﻿using Analytics.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+
+namespace Analytics.Controllers
+{
+    [RoutePrefix("api/sms")]
+    public class SMSController : ApiController
+    {
+        [Route("lote/upload")]
+        [HttpGet]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage LoteUpload()
+        {
+            try
+            {
+                using (SqlHelper sql = new SqlHelper("DB_SMS"))
+                {
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_upload");
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
