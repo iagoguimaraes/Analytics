283b37c876e08c8be145577c8459b7e7a834ad3d
Vihgalvao <victorgabrielsg@gmail.com>
2019-07-04 14:55:10 -0300

    Ajustes sky hora humano.

--- Analytics/Controllers/SkyHumanoController.cs
+++ Analytics/Controllers/SkyHumanoController.cs
@@ -45,10 +45,13 @@ namespace Analytics.Controllers
             {
                 DateTime dtini = Convert.ToDateTime(form["dtini"]);
                 DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+                int horaini = Convert.ToInt16(form["horaini"]);
+                int horafim = Convert.ToInt16(form["horafim"]);
                 DataTable carteira = JsonConvert.DeserializeObject<DataTable>(form["carteira"]);
                 DataTable segmentacao = JsonConvert.DeserializeObject<DataTable>(form["segmentacao"]);
                 DataTable supervisor = JsonConvert.DeserializeObject<DataTable>(form["supervisor"]);
                 DataTable tenure = JsonConvert.DeserializeObject<DataTable>(form["tenure"]);
+                DataTable ocorrencia = JsonConvert.DeserializeObject<DataTable>(form["ocorrencia"]);
 
                 using (SqlHelper sql = new SqlHelper("CUBO_SKY_HUMANO"))
                 {
@@ -60,6 +63,9 @@ namespace Analytics.Controllers
                     parametros.Add("segmentacao", segmentacao);
                     parametros.Add("supervisor", supervisor);
                     parametros.Add("tenure", tenure);
+                    parametros.Add("ocorrencia", ocorrencia);
+                    parametros.Add("horaini", horaini);
+                    parametros.Add("horafim", horafim);
 
                     DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_horahora", parametros);
                     return Request.CreateResponse(HttpStatusCode.OK, resultado);
