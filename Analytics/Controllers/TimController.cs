8297cf419a0bcdf3d20f5a49e59886ec53b3a270
iago.guimaraes <iago.guimaraes@DES05-DIG>
2019-08-09 15:53:58 -0300

    dashboard portabilidade tim vendas

--- Analytics/Controllers/TimController.cs
+++ Analytics/Controllers/TimController.cs
@@ -1856,6 +1856,34 @@ namespace Analytics.Controllers
             }
         }
 
+        [Route("vendas/backoffice/portabilidade")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage VendasBackofficePortabilidade(FormDataCollection form)
+        {
+            try
+            {
+                DateTime dtini = Convert.ToDateTime(form["dtini"]);
+                DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_TIM_VENDAS"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("atom_dashboard_backoffice_portabilidade", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
         #endregion
 
         #region VENDAS GERENCIAL
