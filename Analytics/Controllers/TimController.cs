056c4d5605e85923242ba702a3dcbd432bb92980
iago.guimaraes <iago.guimaraes@DES05-DIG>
2019-09-04 13:02:38 -0300

    dashboard rastreio tim vendas

--- Analytics/Controllers/TimController.cs
+++ Analytics/Controllers/TimController.cs
@@ -1904,6 +1904,35 @@ namespace Analytics.Controllers
             }
         }
 
+
+        [Route("vendas/backoffice/rastreio")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage VendasBackofficeRastreio(FormDataCollection form)
+        {
+            try
+            {
+                DateTime dtini = Convert.ToDateTime(form["dtini"]);
+                DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_TIM_VENDAS"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_rastreio", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
         #endregion
 
         #region VENDAS GERENCIAL
