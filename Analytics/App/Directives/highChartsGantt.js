3d186bb25160f27d7816244bcc627d3320bd25ac
Vihgalvao <victorgabrielsg@gmail.com>
2019-10-31 18:18:29 -0300

    Gantt.

---
+++ Analytics/App/Directives/highChartsGantt.js
@@ -0,0 +1,16 @@
+﻿angular.module('app').directive('highChartsGantt', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            config: '=',
+        },
+        template: '<div></div>',
+        link: function (scope, element) {
+            scope.$watch('config', function () {
+                Highcharts.ganttChart(element[0], scope.config);
+            });
+        }
+    }
+});
+
