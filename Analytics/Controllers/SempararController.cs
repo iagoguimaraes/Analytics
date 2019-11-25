af786230559fd20dd027d737ebfea1ad22cf2435
Dácio Silva <dacio.silva@creditcash.com.br>
2019-11-25 17:45:41 -0200

    dash comparativo sem parar hum

--- Analytics/Controllers/SempararController.cs
+++ Analytics/Controllers/SempararController.cs
@@ -341,6 +341,86 @@ namespace Analytics.Controllers
             }
         }
 
+        [Route("dashboard/humano/comparativo")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage DashboardComparativo(FormDataCollection form)
+        {
+            try
+            {
+                DateTime dtini = Convert.ToDateTime(form["dtini"]);
+                DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+                DateTime dtini_2 = Convert.ToDateTime(form["dtini_2"]);
+                DateTime dtfim_2 = Convert.ToDateTime(form["dtfim_2"]);
+
+                int horaini = Convert.ToInt16(form["horaini"]);
+                int horafim = Convert.ToInt16(form["horafim"]);
+
+                int horaini_2 = Convert.ToInt16(form["horaini_2"]);
+                int horafim_2 = Convert.ToInt16(form["horafim_2"]);
+
+                DataTable empresas = JsonConvert.DeserializeObject<DataTable>(form["empresas"]);
+                DataTable carteiras = JsonConvert.DeserializeObject<DataTable>(form["carteiras"]);
+                DataTable faixaAtraso = JsonConvert.DeserializeObject<DataTable>(form["faixaAtraso"]);
+                DataTable plano = JsonConvert.DeserializeObject<DataTable>(form["plano"]);
+                DataTable devedor = JsonConvert.DeserializeObject<DataTable>(form["devedor"]);
+
+                DataTable empresas_2 = JsonConvert.DeserializeObject<DataTable>(form["empresas_2"]);
+                DataTable carteiras_2 = JsonConvert.DeserializeObject<DataTable>(form["carteiras_2"]);
+                DataTable faixaAtraso_2 = JsonConvert.DeserializeObject<DataTable>(form["faixaAtraso_2"]);
+                DataTable plano_2 = JsonConvert.DeserializeObject<DataTable>(form["plano_2"]);
+                DataTable devedor_2 = JsonConvert.DeserializeObject<DataTable>(form["devedor_2"]);
+
+                string procedure = "sp_dashboard_comparativo_hora";
+                if (form["visao"] == "hora")
+                    procedure = "sp_dashboard_comparativo_hora";
+                if (form["visao"] == "dia")
+                    procedure = "sp_dashboard_comparativo_dia";
+                if (form["visao"] == "dia_semana")
+                    procedure = "sp_dashboard_comparativo_dia_semana";
+                if (form["visao"] == "semana")
+                    procedure = "sp_dashboard_comparativo_semana";
+                if (form["visao"] == "mes")
+                    procedure = "sp_dashboard_comparativo_mes";
+
+                using (SqlHelper sql = new SqlHelper("CUBO_SEMPARAR"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtini_2", dtini_2.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim_2", dtfim_2.ToString("yyyy-MM-dd"));
+
+                    parametros.Add("horaini", horaini);
+                    parametros.Add("horafim", horafim);
+
+                    parametros.Add("horaini_2", horaini_2);
+                    parametros.Add("horafim_2", horafim_2);
+
+                    parametros.Add("empresas", empresas);
+                    parametros.Add("carteiras", carteiras);
+                    parametros.Add("faixaAtraso", faixaAtraso);
+                    parametros.Add("plano", plano);
+                    parametros.Add("devedor", devedor);
+
+                    parametros.Add("empresas_2", empresas_2);
+                    parametros.Add("carteiras_2", carteiras_2);
+                    parametros.Add("faixaAtraso_2", faixaAtraso_2);
+                    parametros.Add("plano_2", plano_2);
+                    parametros.Add("devedor_2", devedor_2);
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet(procedure, parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
         #endregion
 
         #region DIGITAL
