6acb95d4b8d28321c922fa1c77586c331e03baf8
Guilherme Fernandes Rocha <gui_fernandes07@hotmail.com>
2019-12-03 11:56:37 -0300

    dash uol humano

--- Analytics/Controllers/UolController.cs
+++ Analytics/Controllers/UolController.cs
@@ -58,7 +58,7 @@ namespace Analytics.Controllers
 
                     parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
                     parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
-                  
+
 
                     DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_horahora", parametros);
                     return Request.CreateResponse(HttpStatusCode.OK, resultado);
@@ -305,6 +305,11 @@ namespace Analytics.Controllers
                 DateTime data = Convert.ToDateTime(form["dtini"]);
                 DataTable segmentacao = JsonConvert.DeserializeObject<DataTable>(form["segmentacao"]);
 
+                string procedure = "sp_dashboard_baseativa";
+                if (form["visao"] == "digital")
+                    procedure = "sp_dashboard_baseativa";
+                if (form["visao"] == "humano")
+                    procedure = "sp_dashboard_humano_baseativa";
 
                 using (SqlHelper sql = new SqlHelper("CUBO_UOL"))
                 {
@@ -314,7 +319,7 @@ namespace Analytics.Controllers
                     parametros.Add("data", data);
                     parametros.Add("segmentacao", segmentacao);
 
-                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_baseativa", parametros);
+                    DataSet resultado = sql.ExecuteProcedureDataSet(procedure, parametros);
                     return Request.CreateResponse(HttpStatusCode.OK, resultado);
                 }
             }
@@ -409,6 +414,85 @@ namespace Analytics.Controllers
                     parametros.Add("horafim", horafim);
 
                     DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_humano_producao", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Route("dashboard/humano/comparativo")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage DashboardComparativoProducao(FormDataCollection form)
+        {
+            try
+            {
+                DateTime dtini = Convert.ToDateTime(form["dtini"]);
+                DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+                DateTime dtini_2 = Convert.ToDateTime(form["dtini_2"]);
+                DateTime dtfim_2 = Convert.ToDateTime(form["dtfim_2"]);
+
+                int horaini = Convert.ToInt16(form["horaini"]);
+                int horafim = Convert.ToInt16(form["horafim"]);
+
+                int horaini_2 = Convert.ToInt16(form["horaini_2"]);
+                int horafim_2 = Convert.ToInt16(form["horafim_2"]);
+
+
+                string procedure = "sp_dashboard_humano_comparativo_hora";
+
+                if (form["visao"] == "hora")
+                    procedure = "sp_dashboard_humano_comparativo_hora";
+                if (form["visao"] == "dia")
+                    procedure = "sp_dashboard_humano_comparativo_dia";
+                if (form["visao"] == "dia_semana")
+                    procedure = "sp_dashboard_humano_comparativo_dia_semana";
+                if (form["visao"] == "semana")
+                    procedure = "sp_dashboard_humano_comparativo_semana";
+                if (form["visao"] == "mes")
+                    procedure = "sp_dashboard_humano_comparativo_mes";
+
+                using (SqlHelper sql = new SqlHelper("CUBO_UOL"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtini_2", dtini_2.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim_2", dtfim_2.ToString("yyyy-MM-dd"));
+
+                    parametros.Add("horaini", horaini);
+                    parametros.Add("horafim", horafim);
+
+                    parametros.Add("horaini_2", horaini_2);
+                    parametros.Add("horafim_2", horafim_2);
+
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet(procedure, parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Route("dashboard/humano/projecao")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage DashboardProjecaoHumano(FormDataCollection form)
+        {
+            try
+            {
+                using (SqlHelper sql = new SqlHelper("CUBO_UOL"))
+                {
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_humano_projecao");
                     return Request.CreateResponse(HttpStatusCode.OK, resultado);
                 }
             }
