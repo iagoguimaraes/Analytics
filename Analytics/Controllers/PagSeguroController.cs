bb3b949433208b8461aaa0f400c3915f90599acd
Guilherme Fernandes Rocha <gui_fernandes07@hotmail.com>
2020-01-24 16:19:39 -0300

    PagSeguro: dash pagamento

--- Analytics/Controllers/PagSeguroController.cs
+++ Analytics/Controllers/PagSeguroController.cs
@@ -325,6 +325,33 @@ namespace Analytics.Controllers
             }
         }
 
+        [Route("dashboard/pagamento")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage DashboardPagamentoHumano(FormDataCollection form)
+        {
+            try
+            {
+                DateTime dtini = Convert.ToDateTime(form["dtini"]);
+                DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_PAGSEGURO"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_pagamento", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
 
         #endregion
 
