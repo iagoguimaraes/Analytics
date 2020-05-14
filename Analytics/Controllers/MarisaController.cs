1aa37b70330ae7ad005645a23a36634d84a10d4d
iago.guimaraes <iago.guimaraes@DES05-DIG>
2020-05-14 15:40:20 -0300

    marisa sinergy

--- Analytics/Controllers/MarisaController.cs
+++ Analytics/Controllers/MarisaController.cs
@@ -1222,6 +1222,34 @@ namespace Analytics.Controllers
             }
         }
 
+        [Route("dashboard/sinergy/producao")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage DashboardProducaoSinergy(FormDataCollection form)
+        {
+            try
+            {
+                DateTime dtini = Convert.ToDateTime(form["dtini"]);
+                DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_MARISA_DIGITAL"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_producao_sinergy", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
         [Route("dashboard/digital/filtros")]
         [HttpGet]
         [Autorizar]
