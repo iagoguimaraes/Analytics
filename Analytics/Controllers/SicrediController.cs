f7170779fc73d7f837a7a0dceec951b910c65b36
Guilherme Fernandes Rocha <guilherme.rocha@creditcash.com.br>
2020-08-03 12:18:41 -0300

    dash sicredi - btc

--- Analytics/Controllers/SicrediController.cs
+++ Analytics/Controllers/SicrediController.cs
@@ -425,6 +425,36 @@ namespace Analytics.Controllers
             }
         }
 
+        [Route("dashboard/digital/btc")]
+        [HttpPost]
+        [Autorizar]
+        [Gravar]
+        public HttpResponseMessage DashboardBTC(FormDataCollection form)
+        {
+            try
+            {
+                DateTime dtini = Convert.ToDateTime(form["dtini"]);
+                DateTime dtfim = Convert.ToDateTime(form["dtfim"]);
+                DataTable fila = JsonConvert.DeserializeObject<DataTable>(form["fila"]);
+
+                using (SqlHelper sql = new SqlHelper("CUBO_SICREDI_DIGITAL"))
+                {
+                    Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+                    parametros.Add("dtini", dtini.ToString("yyyy-MM-dd"));
+                    parametros.Add("dtfim", dtfim.ToString("yyyy-MM-dd"));
+                    parametros.Add("fila", fila);
+
+                    DataSet resultado = sql.ExecuteProcedureDataSet("sp_dashboard_btc", parametros);
+                    return Request.CreateResponse(HttpStatusCode.OK, resultado);
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
         #endregion
 
     }
